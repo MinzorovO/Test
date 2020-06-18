9ee7b036e91518d2d8c7eec92aaa7146736d732f
minzo <minzo@Minzorov-Aleksandr.mshome.net>
2020-06-18 18:00:24 +0200

    update 18/06/2020 ver2

--- my_test1/src/my_test1/Max-Planck-Project.clj
+++ my_test1/src/my_test1/Max-Planck-Project.clj
@@ -1,34 +1,5 @@
 ;(test_sm Xvector Yvector MassVec)
 (ns my-test1)
-;начало блока удаления данных из массива 
-(defn index-exclude [r ex] 
-  "Take all indices execpted ex" 
-  (filter #(not (ex %)) (range r))) 
-(defn dissoc-idx [v & ds]
-  (map v (index-exclude (count v) (into #{} ds))))
-; конец блока
-;Нахождение индекса указанного значения
-(defn indices [pred coll]
-  (keep-indexed #(when (pred %2) %1) coll)
-  )
-;Нахождение и удаление первого "0"
-(defn findAndDelZero [inpVec]
-  (dissoc-idx
-    inpVec
-    (first (indices zero? inpVec))
-    )
-  )
-;Нахождение индекса минимального значения
-(defn findIndxOfMin [inpVec]
-  (.indexOf 
-    inpVec 
-    (apply min (findAndDelZero inpVec))
-    )
-  )
-;вычитание значений в скобках и возведение их содержимого в квадрат
-(defn x1-x2Sqr[inpVec]
-  (map (fn [x] (map (fn [y] (Math/pow (- x y) 2)) inpVec))inpVec)
-  )
 ; Вычисление радиуса шарика из его массы и плотности
 ; Calculation of the ball radius from its mass and density
 ; m_ball - mass 
@@ -56,12 +27,10 @@
     0
     )
   )
-
 ; Вычисление скорости
 (defn Speed [t_step v_current m_ball LJpow]
   ( + v_current (* (/ LJpow m_ball) t_step))
   )
-
 ; Вычисление координаты Х 
 (defn coordCulc [t_step x_current m_ball speed LJpow]
   (+
@@ -72,16 +41,13 @@
 ;
 ;;;
 (defn newFN [inData]
-  
   (def Data inData)
-  
   (def particleData [
                      {
-                      :coord[0 1 2]
+                      :coord[0 0 0]
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
-                      :tempDist [10 10 10 10]
                       }
                      
                      {
@@ -89,7 +55,6 @@
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
-                      :tempDist [0 0 0 0]
                       }
                      
                      {
@@ -97,7 +62,6 @@
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
-                      :tempDist [0 0 0 0]
                       }
                      
                      {
@@ -105,149 +69,43 @@
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
-                      :tempDist [0 0 0 0]
                       }
                      ])
-  
-  
-  (println "CHENGED")
   
   (while (> (Data 4) 0)
     
     (doseq [[i] (map list (range(count particleData)))]
       (doseq [[j] (map list (range(count particleData)))]
         
-        (def particleDataCopy (assoc (particleData i) :curLJPower (+ ((particleData i) :curLJPower) (LennardJonesPower (Data 2) (Data 3) 
-                                                                                                                       (Math/sqrt(+
+     (def particleDataCopy (assoc (particleData i) :curLJPower (+ ((particleData i) :curLJPower) (LennardJonesPower (Data 2) (Data 3) 
+                                                                                                                    (Math/sqrt(+
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)0) (((get (vec particleData) j):coord)0))2)
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)1) (((get (vec particleData) j):coord)1))2)
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)2) (((get (vec particleData) j):coord)2))2)))
                                                                                                                        ))))
-        (def particleData (assoc particleData i particleDataCopy))
-        )
+     (def particleData (assoc particleData i particleDataCopy))
+     )
       
-      (def particleDataCopy (assoc (particleData i) :curSpeed (Speed (Data 0) ((particleData i) :curSpeed) 
+   (def particleDataCopy (assoc (particleData i) :curSpeed (Speed (Data 0) ((particleData i) :curSpeed) 
                                                                      ((particleData i) :mass) ((particleData i) :curLJPower)
-                                                                     )))
-      ;(doseq [[j] (map list (range(-(count particleData)1)))]
-        ;(println i j) 
-        ;coordCulc [t_step x_current m_ball speed LJpow]
-        (def particleDataCopy (assoc (particleData i):coord  [(coordCulc (Data 0) (((particleData i):coord)0) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
-                                                              (coordCulc (Data 0) (((particleData i):coord)1) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
-                                                              (coordCulc (Data 0) (((particleData i):coord)2) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))]
-                                     )) 
-       ; )
-      
+                                                                     
+                                                                     
+                                                                  )
+                                :coord  [(coordCulc (Data 0) (((particleData i):coord)0) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
+                                            (coordCulc (Data 0) (((particleData i):coord)1) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
+                                            (coordCulc (Data 0) (((particleData i):coord)2) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))]
+                                ))      
       (def particleData (assoc particleData i particleDataCopy))
-      )
-    
-    
-    (println particleData)            
-    
-    (doseq [[i] (map list (range(count particleData)))]   
+   )
+      
+  (doseq [[i] (map list (range(count particleData)))]   
       (def particleDataCopy (assoc (particleData i) :curLJPower 0 ))
       (def particleData (assoc particleData i particleDataCopy))
       )
-    
-    (println Data)
+  
     (def Data (assoc Data 4 (- (Data 4) (Data 0))))
     )
+  (println particleData) 
   )
-
-
-
-
-
-
-
-
-;        (println 
-;          (LennardJonesPower
-;            (Data 2)
-;(Math/sqrt(+
-;            (Math/pow(-(((get (vec particleData) i):coord)0) (((get (vec particleData) j):coord)0))2)
-;            (Math/pow(-(((get (vec particleData) i):coord)1) (((get (vec particleData) j):coord)1))2)
-;            (Math/pow(-(((get (vec particleData) i):coord)2) (((get (vec particleData) j):coord)2))2)))
-;            (Data 3)
-;            )
-;          )
-
-
-
-
-
-
-
-;  (def particleData {:coordX [13 20 15 18]
-;                     :coordY [0 12 7 4]
-;                     :coordZ [4 8 17 1]
-;                     :massPart [5 10 15 20]
-;                     :tempDist [999 999 999]
-;                     :realDist [999 999 999 999]
-;                     :vCur [0 0 0 0]
-;                     })
-;
-;(while (>(get inData 4) (get inData 0))
-;    
-;    
-;    (doseq [[x y z i] (map list (x1-x2Sqr(vec(particleData :coordX))) 
-;                           (x1-x2Sqr(vec(particleData :coordY)))      
-;                           (x1-x2Sqr(vec(particleData :coordZ)))      
-;                           (range (count(particleData :coordX))))]    
-;      
-;      (doseq [[k] (map list (range(count(findAndDelZero (vec z)))))]
-;        (def particleData (assoc particleData   
-;                                 :tempDist  (assoc (particleData :tempDist) k  
-;                                                   
-;                                                   (Math/sqrt (+
-;                                                                (get (vec(findAndDelZero (vec x))) k)
-;                                                                (get (vec(findAndDelZero (vec y))) k)
-;                                                                (get (vec(findAndDelZero (vec z))) k)))
-;                                                   )
-;                                 )))
-;      
-;      (if (>
-;            (apply min (particleData :tempDist))
-;            (* (Math/pow 2 (/ 1 6)) (get inData 3))
-;            )
-;        
-;        (def particleData (assoc particleData 
-;                                 :coordX  (assoc (particleData :coordX) i (coordCulc  
-;                                                                            (get inData 0) (get (particleData :coordX) i) (get (particleData :massPart) i) 
-;                                                                            (get(particleData :vCur)i) (get inData 2) (apply min (particleData :tempDist)) (get inData 3)
-;                                                                            ))
-;                                 :coordY (assoc (particleData :coordY) i (coordCulc  
-;                                                                           (get inData 0) (get (particleData :coordY) i) (get (particleData :massPart) i) 
-;                                                                           (get(particleData :vCur)i) (get inData 2) (apply min (particleData :tempDist)) (get inData 3)
-;                                                                           ))
-;                                 :coordZ (assoc (particleData :coordZ) i (coordCulc  
-;                                                                           (get inData 0) (get (particleData :coordZ) i) (get (particleData :massPart) i) 
-;                                                                           (get(particleData :vCur)i) (get inData 2) (apply min (particleData :tempDist)) (get inData 3)
-;                                                                           ))
-;                                 ))
-;        )
-;      
-;      (def particleData (assoc particleData 
-;                               :realDist  (assoc (particleData :realDist) i  
-;                                                (Math/sqrt (+
-;                                                             (get (particleData :coordX) i)
-;                                                             (get (particleData :coordY) i)
-;                                                             (get (particleData :coordZ) i)
-;                                                             ))
-;                                                )
-;                               :vCur (assoc (particleData :vCur) i (Speed (get inData 0) (get(particleData :vCur)i) (get (particleData :massPart) i) 
-;                                                                          (get inData 2) (apply min (particleData :tempDist)) (get inData 3)))
-;                               ))
-;      )
-;    )
-
-
-
-
-
-
-
-
 ;        time_s0 v1 e2 q3 time_max4
-;11 -25
 ;(newFN [0.1 0 1 10 1]) 
