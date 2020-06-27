d42545eaf972f327fa3ab0ce3bb14db2c16b1d11
minzo <minzo@Minzorov-Aleksandr.mshome.net>
2020-06-27 16:40:07 +0200

    update 27/06/2020

--- my_test1/src/my_test1/Max-Planck-Project.clj
+++ my_test1/src/my_test1/Max-Planck-Project.clj
@@ -1,5 +1,89 @@
-;(test_sm Xvector Yvector MassVec)
-(ns my-test1)
+(ns my-test1
+(:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(def screen-width 1500)
+(def screen-height 800)
+
+(defn funk [data]
+  (q/clear)
+  (q/camera 700 700 700 0 0 0 0 0 -1)
+  
+  ; draw red X axis
+  (q/stroke 255 0 0)
+  (q/line 0 0 0 1000 0 0)
+  ; draw green Y axis
+  (q/stroke 0 255 0)
+  (q/line 0 0 0 0 1000 0)
+  ; draw blue Z axis
+  (q/stroke 0 0 255)
+  (q/line 0 0 0 0 0 1000)
+  
+  (q/sphere-detail 15)
+  (q/with-translation ((data 0) :coord)
+    (q/sphere 5))
+  
+  (q/sphere-detail 15)
+  (q/with-translation ((data 1) :coord)
+    (q/sphere 5))
+  
+  (q/sphere-detail 15)
+  (q/with-translation ((data 2) :coord)
+    (q/sphere 10))
+  
+  
+  (q/sphere-detail 15)
+  (q/with-translation ((data 3) :coord)
+    (q/sphere 5))
+  
+  )
+
+(defn draw [state]
+  (def particleData [
+                     {
+                      :coord[0 0 0]
+                      :mass 5
+                      :curLJPower 0
+                      :curSpeed 0
+                      }
+                     {
+                      :coord[13 13 13]
+                      :mass 5
+                      :curLJPower 0
+                      :curSpeed 0
+                      }
+                     {
+                      :coord[13 0 0]
+                      :mass 5
+                      :curLJPower 0
+                      :curSpeed 0
+                      }
+                     {
+                      :coord[0 13 0]
+                      :mass 5
+                      :curLJPower 0
+                      :curSpeed 0
+                      }
+                     ])
+  
+    (let [t (/ (q/frame-count) 25)]
+      (funk particleData))
+    )
+  
+
+(defn setup []
+  (q/color-mode :rgb)
+  (q/frame-rate 60)
+  {})
+
+(defn -main [x]
+  (q/defsketch quil-experiments
+    :size [screen-width screen-height]
+    :setup setup
+    :draw draw 
+    :middleware [m/fun-mode]
+    :renderer :opengl))
+
 ; Вычисление радиуса шарика из его массы и плотности
 ; Calculation of the ball radius from its mass and density
 ; m_ball - mass 
@@ -15,6 +99,7 @@
 ; r - the distance between the centers of the particles
 ; q - the distance at which the interaction energy becomes zero
 (defn LennardJonesPower [e q r]
+  (println r)
   (if (> r 0)
     (if (<= r (* (Math/pow 2 (/ 1 6)) q))
       -0.2
@@ -33,10 +118,7 @@
   )
 ; Вычисление координаты Х 
 (defn coordCulc [t_step x_current m_ball speed LJpow]
-  (+
-    (+ x_current (* speed t_step))
-    (* (/ LJpow m_ball) (/(Math/pow t_step 2)2))
-    )
+  (+ x_current (* speed t_step) (* (/ LJpow m_ball) (/(Math/pow t_step 2)2)))
   )
 ;
 ;;;
@@ -49,23 +131,20 @@
                       :curLJPower 0
                       :curSpeed 0
                       }
-                     
                      {
                       :coord[13 13 13]
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
                       }
-                     
                      {
-                      :coord[5 5 5]
+                      :coord[13 0 0]
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
                       }
-                     
                      {
-                      :coord[7 7 7]
+                      :coord[0 13 0]
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
@@ -75,33 +154,36 @@
   (while (> (Data 4) 0)
     
     (doseq [[i] (map list (range(count particleData)))]
-      (doseq [[j] (map list (range(count particleData)))]
+      (doseq [[j] (map list (range(count particleData)))]  
         
-     (def particleDataCopy (assoc (particleData i) :curLJPower (+ ((particleData i) :curLJPower) (LennardJonesPower (Data 2) (Data 3) 
-                                                                                                                    (Math/sqrt(+
+        (def particleDataCopy (assoc (particleData i) :curLJPower (+ ((particleData i) :curLJPower) (LennardJonesPower (Data 2) (Data 3) 
+                                                                                                                       (Math/sqrt(+
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)0) (((get (vec particleData) j):coord)0))2)
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)1) (((get (vec particleData) j):coord)1))2)
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)2) (((get (vec particleData) j):coord)2))2)))
                                                                                                                        ))))
-     (def particleData (assoc particleData i particleDataCopy))
-     )
-      
-   (def particleDataCopy (assoc (particleData i) :curSpeed (Math/sqrt(Math/pow(Speed (Data 0) ((particleData i) :curSpeed) 
-                                                                     ((particleData i) :mass) ((particleData i) :curLJPower) 
-                                                                  )2))
-                                :coord  [(coordCulc (Data 0) (((particleData i):coord)0) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
+        (def particleData (assoc particleData i particleDataCopy))
+        )
+      )
+    
+    (doseq [[i] (map list (range(count particleData)))]
+      (def particleDataCopy (assoc (particleData i) :curSpeed (Math/sqrt(Math/pow(Speed (Data 0) ((particleData i) :curSpeed) 
+                                                                                        ((particleData i) :mass) ((particleData i) :curLJPower) )2))
+                                   
+                                   :coord  [(coordCulc (Data 0) (((particleData i):coord)0) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
                                             (coordCulc (Data 0) (((particleData i):coord)1) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
                                             (coordCulc (Data 0) (((particleData i):coord)2) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))]
-                                ))      
+                                   ))      
       (def particleData (assoc particleData i particleDataCopy))
-   )
-      
-  (doseq [[i] (map list (range(count particleData)))]   
+      )   
+    
+    (doseq [[i] (map list (range(count particleData)))]   
       (def particleDataCopy (assoc (particleData i) :curLJPower 0
-                                   :curSpeed 0))
+                                   :curSpeed 0
+                                   ))
       (def particleData (assoc particleData i particleDataCopy))
       )
-  
+    
     (def Data (assoc Data 4 (- (Data 4) (Data 0))))
     )
   (println particleData) 
