4c77985ecf4760d54d4b329c72630b81fc2533fc
minzo <minzo@Minzorov-Aleksandr.mshome.net>
2020-07-03 22:24:17 +0200

    update 03/07/2020

--- my_test1/src/my_test1/Max-Planck-Project.clj
+++ my_test1/src/my_test1/Max-Planck-Project.clj
@@ -1,5 +1,5 @@
 (ns my-test1
-(:require [quil.core :as q]
+  (:require [quil.core :as q]
             [quil.middleware :as m]))
 
 ; Вычисление силы взаимодействия шариков используя потенциал Ленарда-Джонса
@@ -8,13 +8,14 @@
 ; r - the distance between the centers of the particles
 ; q - the distance at which the interaction energy becomes zero
 (defn LennardJonesPower [e q r]
+  (println r)
   (if (> r 0)
     (if (<= r (* (Math/pow 2 (/ 1 6)) q))
-      -0.5
+      -0.001
       (if (<= r (* 2.5 q)) 
         (-(* (/ (* 12 e) q)(-(Math/pow (/ q r) 13)(Math/pow (/ q r) 7)))
           (* (/ (* 12 e) q)(-(Math/pow (/ q (* 2.5 q)) 13)(Math/pow (/ q (* 2.5 q)) 7))))
-        0.0002    
+        0    
         )
       )
     0
@@ -22,18 +23,19 @@
   )
 ; Вычисление скорости
 (defn Speed [t_step v_current m_ball LJpow]
-  ( + v_current (* (/ LJpow m_ball) t_step))
+  ( + 
+    v_current 
+    (* (/ LJpow m_ball) t_step)
+    )
   )
 ; Вычисление координаты Х 
-(defn coordCulc [t_step x_current m_ball speed LJpow]
-  (+ x_current (* speed t_step) (* (/ LJpow m_ball) (/(Math/pow t_step 2)2)))
+(defn coordCulc [t_step cord_current m_ball speed LJpow]
+  (+ cord_current (* speed t_step) (* (/ LJpow m_ball) (/(Math/pow t_step 2)2)))
   )
-
-
+;
 (defn funk [data]
   (q/clear)
   (q/camera 50 50 50 0 0 0 0 0 -1)
-  
   ; draw red X axis
   (q/stroke 255 0 0)
   (q/line 0 0 0 1000 0 0)
@@ -52,26 +54,25 @@
   (q/with-translation ((data 1) :coord)
     (q/sphere ((data 1) :mass)))
   
-;  (q/sphere-detail 15)
-;  (q/with-translation ((data 2) :coord)
-;    (q/sphere ((data 2) :mass)))
-;  
-;  
-;  (q/sphere-detail 15)
-;  (q/with-translation ((data 3) :coord)
-;    (q/sphere ((data 3) :mass)))
+  ;  (q/sphere-detail 15)
+  ;  (q/with-translation ((data 2) :coord)
+  ;    (q/sphere ((data 2) :mass)))
+  ;  
+  ;  
+  ;  (q/sphere-detail 15)
+  ;  (q/with-translation ((data 3) :coord)
+  ;    (q/sphere ((data 3) :mass)))
   
   )
-
+;
 (defn draw [state]
   
   (let [t (/ (q/frame-count) 25)]
     ;        time_step-0 v-1 e-2 q-3
-    (def Data [1 0 11 100])
+    (def Data [1 0 11 65])
     
     (doseq [[i] (map list (range(count particleData)))]
       (doseq [[j] (map list (range(count particleData)))]  
-        
         (def particleDataCopy (assoc (particleData i) :curLJPower (+ ((particleData i) :curLJPower) (LennardJonesPower (Data 2) (Data 3) 
                                                                                                                        (Math/sqrt(+
                                                                                                                                    (Math/pow(-(((get (vec particleData) i):coord)0) (((get (vec particleData) j):coord)0))2)
@@ -84,28 +85,26 @@
     
     (doseq [[i] (map list (range(count particleData)))]
       (def particleDataCopy (assoc (particleData i) :curSpeed (Speed (Data 0) ((particleData i) :curSpeed) 
-                                                                                        ((particleData i) :mass) ((particleData i) :curLJPower))
+                                                                     ((particleData i) :mass) ((particleData i) :curLJPower))
                                    
                                    :coord  [(coordCulc (Data 0) (((particleData i):coord)0) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
                                             (coordCulc (Data 0) (((particleData i):coord)1) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))
-                                            (coordCulc (Data 0) (((particleData i):coord)2) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))]
-                                   ))      
+                                            (coordCulc (Data 0) (((particleData i):coord)2) ((particleData i) :mass) ((particleData i) :curSpeed) ((particleData i) :curLJPower))]))      
       (def particleData (assoc particleData i particleDataCopy))
       )   
     
     (doseq [[i] (map list (range(count particleData)))]   
-      (def particleDataCopy (assoc (particleData i) :curLJPower 0
-                                   :curSpeed 0
-                                   ))
+      (def particleDataCopy (assoc (particleData i) 
+                                   :curLJPower 0
+                                   :curSpeed 0))
       (def particleData (assoc particleData i particleDataCopy))
       )
-    
+    (println particleData)
     (funk particleData)
     
     )
   )
-
-
+;
 (defn setup []
   (q/color-mode :rgb)
   (q/frame-rate 60)
@@ -117,14 +116,14 @@
                       :curSpeed 0
                       }
                      {
-                      :coord[10 20 0]
+                      :coord[0 20 10]
                       :mass 5
                       :curLJPower 0
                       :curSpeed 0
                       }
                      ])
   {})
-
+;
 (defn -main [x]
   (q/defsketch quil-experiments
     :size [700 700]
